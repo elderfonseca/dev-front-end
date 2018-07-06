87716938d89b7157c2d0b8796aa80138c4ba3185
Elder Fonseca <elder.fonseca15@gmail.com>
2018-07-06 11:55:59 -0300

    Criação dos arquivos para o Gulp e criação do CSS de fontes

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+
+/*
+ * Variaveis
+ */
+// Arquivo Sass
+var scssFiles = './src/css/fonts.scss';
+
+// Destino do CSS
+var cssDest = './src/css';
+
+// Opções para desenvolvimento
+var sassDevOptions = {
+  outputStyle: 'expanded'
+}
+
+// Opções para produção
+var sassProdOptions = {
+  outputStyle: 'compressed'
+}
+
+/*
+ * Tarefas
+ */
+// Tarefa 'sassdev' - Roda com o comando 'gulp sassdev'
+gulp.task('sassdev', function() {
+  return gulp.src(scssFiles)
+    .pipe(sass(sassDevOptions).on('error', sass.logError))
+    .pipe(gulp.dest(cssDest));
+});
+
+// Tarefa 'sassprod' - Roda com o comando 'gulp sassprod'
+gulp.task('sassprod', function() {
+  return gulp.src(scssFiles)
+    .pipe(sass(sassProdOptions).on('error', sass.logError))
+    .pipe(rename('fonts.min.css'))
+    .pipe(gulp.dest(cssDest));
+});
+
+// Tarefa 'watch' - Roda com o comando 'gulp watch'
+gulp.task('watch', function() {
+  gulp.watch(scssFiles, ['sassdev', 'sassprod']);
+});
+
+// Tarefa Default - Roda com o comando 'gulp'
+gulp.task('default', ['sassdev', 'sassprod', 'watch']);
